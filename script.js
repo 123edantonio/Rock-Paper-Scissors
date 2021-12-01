6c5d48f9f3064a719add8a9d32695224d4cdc413
Elizabeth D'Antonio <123edantonio@frhsd.com>
2021-12-01 08:36:45 -0500

    commit5

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let userChoice; //get value from RPS choice
 let compChoice; //random RPS choice
 let userScore = 0, compScore = 0; //keep track of wins and losses
+let resultMessage;
 //maybe a result variable
 
 //startup functionality *not a in function*
@@ -22,32 +23,68 @@ function getInput(choice)
 {
   userChoice = choice;
   //console.log(choice);
+  function getRandomChoice()
 }
 
 //get random/computer choice of rockpaperscissors
 function getRandomChoice()
 {
-  //randomly choose one of RPS
+  compChoice = Math.floor(Math.random() * 3); //set compChoice equal to 0 1 or 2
+  // 0 is rock 1 is paper 2 is Scissors
+  //console.log(compChoice);
+
 }
 
 //comparing user vs comp comp choice
 function compareChoices()
 {
   //switch statement for ALL possibilities
+  switch (userChoice + compChoice)
+{
+case "scissors2":
+case "paper1":
+case "rock0":
+  resultMessage = "Draw!";
+  break;
+case "scissors0":
+case "paper2":
+case "rock1":
+  resultMessage = "You Lose";
+  updateScore("comp");
+  break;
+case "scissors1":
+case "rock0":
+case "paper2":
+  resultMessage = "You Win!";
+  updateScore("user");
+  break;
+  default:
+  resultMessage = "something went wrong";
 }
 
+displayResult();}
 //display round winner
 function displayResult()
 {
-  //get HTML element to display the text
-  //assign string value
+  document.getElementById("result").textContent = resultMessage;;
+  //get HTML element to display the text and assign resultMessage string
 }
 
 //update score
-function updateScore()
+function updateScore(winner)
 {
+  if(winner == "user")
+  {
+    compScore++;
+  }
+  else {
+    {
+      compScore++;
+    }
+  }
   //increment user or somp comp score based on round winner
   //update score on screen
+  console.log(compScore + " : " + userScore)
 }
 
 //reset score(!!Add a reset score button!!)
